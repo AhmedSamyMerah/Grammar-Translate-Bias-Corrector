f56810918490a7db8d3572c5864ad21128ffa0df
AhmedSamyMerah <meraha@sheridancollege.ca>
2020-09-09 18:42:58 -0400

    created substitution for words ending in on

--- textCorrection.py
+++ textCorrection.py
@@ -24,10 +24,25 @@ def gender_corrector(trText):
     index =None
     hv= None
 
+    #Pronoun check
     if('elle' in s2):
+        #Loop to check if a noun is masculine
         for n in ds1:
             if(n.pos_ == 'NOUN'):
                 if('Gender=Masc' in n.tag_):
+                    #get index of token
                     index=n.i
+                    #Stringify token
                     dn = str(n)
                     sn = str(n)
+
+                    if(sn[-2:] == 'on'):
+                        sn = sn.replace('on', 'onne')
+                        hv = nlp(sn)
+                    for v in hv:
+                        if(v.has_vector):
+                            print('!!!')
+                            s1 = s1.replace(dn, sn)
+                            print('issa noun', sn)
+                    continue
+    print(s1,s2)
