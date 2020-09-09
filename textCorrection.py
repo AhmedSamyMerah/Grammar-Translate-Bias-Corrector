9b567cc8a1e20a0d5ecea450b8d9c5c0a287decf
AhmedSamyMerah <meraha@sheridancollege.ca>
2020-09-09 18:29:24 -0400

    Broke the Doc object into sentences for analyzing

--- textCorrection.py
+++ textCorrection.py
@@ -10,4 +10,9 @@ def gender_corrector(trText):
     #Doc for processing original GT translation
     doc = nlp(trText)
 
-    
+    sents = list(doc.sents)
+    ds1 = sents[0]
+    ds2 = sents[1]
+    s1 = str(sents[0])
+    s2 = str(sents[1])
+    print(s1, s2)
