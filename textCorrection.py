efe7f1ed7c3117cb3de0155c4884c5537d5475a2
AhmedSamyMerah <meraha@sheridancollege.ca>
2020-09-09 18:35:19 -0400

    added pronoun and noun check for gender

--- textCorrection.py
+++ textCorrection.py
@@ -10,9 +10,24 @@ def gender_corrector(trText):
     #Doc for processing original GT translation
     doc = nlp(trText)
 
+    #Sentence breakdown
     sents = list(doc.sents)
     ds1 = sents[0]
     ds2 = sents[1]
     s1 = str(sents[0])
     s2 = str(sents[1])
     print(s1, s2)
+
+    #Variables for token storing
+    dn =None
+    sn =None
+    index =None
+    hv= None
+
+    if('elle' in s2):
+        for n in ds1:
+            if(n.pos_ == 'NOUN'):
+                if('Gender=Masc' in n.tag_):
+                    index=n.i
+                    dn = str(n)
+                    sn = str(n)
